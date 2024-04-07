bf301b35612a056dc510a7a495edd22ccf3eed66
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-07 02:43:09 -0400

    Implemented login API and test case

---
+++ devume/tests/test_login_view.py
@@ -0,0 +1,28 @@
+
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+
+
+class LoginViewTestCase(APITestCase):
+  # Built in function from Django
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_password'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+
+
+    def test_login(self):
+        user_data = {
+            'username': self.username,
+            'password': self.password,
+        }
+
+        response = self.client.post('/api/login', user_data, format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('token', response.data)
+
+    # Test case tear down procedures
+    # Built in function from Django
+    def tearDown(self):
+        return None
--- devume/tests/test_user_view.py
+++ devume/tests/test_user_view.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User
-from django.test import TestCase, Client
-import json
+from rest_framework.test import APITestCase
 
-class UserViewTestCase(TestCase):
+class UserViewTestCase(APITestCase):
     # Setup test case
     # Built in function from Django
     def setUp(self):
-        self.client = Client()
+        return None
 
     def test_get_users(self):
         response = self.client.get('/api/users')
@@ -19,7 +18,7 @@ class UserViewTestCase(TestCase):
             'email': 'test@example.com'
         }
 
-        response = self.client.post('/api/users/create', json.dumps(user_data), content_type='application/json')
+        response = self.client.post('/api/users/create', user_data,format='json')
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(User.objects.count(), 1)
