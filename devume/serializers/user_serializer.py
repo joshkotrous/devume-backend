c3c227a8205ed7263c429f764a21bda780d89dfd
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-07 00:53:24 -0400

    added make_password function to userserializer

--- devume/serializers/user_serializer.py
+++ devume/serializers/user_serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from django.contrib.auth.hashers import make_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        
+        return super().create(validated_data)
