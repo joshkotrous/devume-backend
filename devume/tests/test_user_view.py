15d12c838164913b42e03bb6bb126ca2e61ae91b
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-07 01:30:40 -0400

    started on user test case

---
+++ devume/tests/test_user_view.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+import json
+
+class UserViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_users(self):
+        response = self.client.get('/api/users')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user(self):
+        user_data = {
+            'username': 'testuser122',
+            'password': 'testPassword',
+            'email': 'test@example.com'
+        }
+
+        response = self.client.post('/api/users/create', json.dumps(user_data), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.first().username, 'testuser122')
