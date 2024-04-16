e4edd793724141582457f7879a3a554417cd3b79
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-15 19:23:25 -0700

    added countries API and models test case

---
+++ devume/views/countries_view.py
@@ -0,0 +1,30 @@
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, CreateAPIView, ListAPIView
+
+from rest_framework.permissions import IsAuthenticated
+from devume.models.country import Country
+from devume.authentication.bearer_authentication import BearerTokenAuthentication
+from devume.serializers.city_serializer import CountrySerializer
+
+class CountriesListView(ListAPIView):
+    authentication_classes = [BearerTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+class CountriesRetrieveView(RetrieveAPIView):
+    authentication_classes = [BearerTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+class CountriesCreateView(CreateAPIView):
+    authentication_classes = [BearerTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+class CountriesUpdateView(UpdateAPIView):
+    authentication_classes = [BearerTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
