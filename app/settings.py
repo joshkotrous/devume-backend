61bee7b83f28d7a8e387b62d41c737bb738d153a
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-17 13:27:58 -0400

    added env for db

--- app/settings.py
+++ app/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': os.getenv('$DATABASE_PASSWORD'),  
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),  
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
