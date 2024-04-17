6ec8a7b2e6603d56625b58274232241358de32fd
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-17 12:54:49 -0400

    Added health check

---
+++ devume/tests/test_health_check_view.py
@@ -0,0 +1,16 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from devume.models.api_key import ApiKey
+
+
+class HealthCheckViewTestCase(APITestCase):
+    def setUp(self):
+        self.username = 'test_user'
+        self.password = 'test_password'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.apikey = ApiKey.objects.create(user=self.user)
+
+    def test_health_check(self):
+        headers = {'x-api-key': str(self.apikey.key)} 
+        response = self.client.get('/api/health', headers=headers)
+        self.assertEqual(response.status_code, 200)
---
+++ devume/views/health_check_view.py
@@ -0,0 +1,31 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.db import connection
+from devume.authentication.api_key_authentication import ApiKeyAuthentication
+
+class HealthCheckView(APIView):
+    authentication_classes = [ApiKeyAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+    # Perform health checks here
+        database_status = self.check_database()
+        if database_status:
+            return Response({'status': 'ok'})
+        else:
+            return Response({'status': 'error'}, status=500)
+
+    def check_database(self):
+        try:
+            # Attempt to execute a simple query to check database connectivity
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT 1")
+                row = cursor.fetchone()
+                if row:
+                    return True
+                else:
+                    return False
+        except Exception as e:
+            # Log the exception or handle it accordingly
+            return False
