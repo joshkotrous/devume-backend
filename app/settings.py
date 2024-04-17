dc4ead5dba9e560cce1801b75e0d0f5f9816e570
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-17 13:01:26 -0400

    updated settings.py

--- app/settings.py
+++ app/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': 5432,
     }
 }
