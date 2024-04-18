de581039025b621bbf43e0a8afd2ec2043403181
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-17 23:43:51 -0400

    updated readme

--- app/settings.py
+++ app/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DATABASE_NAME"),
-        "USER": os.getenv("DATABASE_USERNAME"),
+        "USER": "postgres",
         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
         "HOST": os.getenv("DATABASE_HOST"),
         "PORT": os.getenv("DATABASE_PORT"),
