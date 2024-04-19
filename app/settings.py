f5601a76dae424b6dc0c00b5ab9c3b3c0a0c069c
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-18 22:15:59 -0400

    Test

--- app/settings.py
+++ app/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(%tzg#m1glga%i-mqyq$0(jsovbn2n2$_jw6vk(989vq-&zb!b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["devume-test.eba-zqrpmuez.us-west-2.elasticbeanstalk.com"]
 
 
 # Application definition
