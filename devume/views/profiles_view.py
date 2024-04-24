b3f45375f4e7c7a0ea16b08f6a79c1baa09c177a
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-04-23 20:32:49 -0400

    added bearer token authentication for profiles

--- devume/views/profiles_view.py
+++ devume/views/profiles_view.py
@@ -10,24 +10,33 @@ from rest_framework.permissions import IsAuthenticated
 from devume.models.profile import Profile
 from devume.serializers.profile_serializer import ProfileSerializer
 from devume.authentication.api_key_authentication import ApiKeyAuthentication
+from devume.authentication.bearer_authentication import BearerTokenAuthentication
 
 
 class ProfileListView(ListAPIView):
-    authentication_classes = [SessionAuthentication, ApiKeyAuthentication]
+    authentication_classes = [
+        SessionAuthentication,
+        ApiKeyAuthentication,
+        BearerTokenAuthentication,
+    ]
     permission_classes = [IsAuthenticated]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class ProfileRetrieveView(RetrieveAPIView):
-    authentication_classes = [SessionAuthentication, ApiKeyAuthentication]
+    authentication_classes = [
+        SessionAuthentication,
+        ApiKeyAuthentication,
+        BearerTokenAuthentication,
+    ]
     permission_classes = [IsAuthenticated]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class ProfileUpdateView(UpdateAPIView):
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [SessionAuthentication, BearerTokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
