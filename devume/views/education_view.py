764ea493c6fcb7cc34c68250605673fc49292911
joshkotrous <43544333+joshkotrous@users.noreply.github.com>
2024-05-19 21:16:14 -0400

    fixed education create view

--- devume/views/education_view.py
+++ devume/views/education_view.py
@@ -10,6 +10,7 @@ from devume.models.education import Education
 from devume.models.profile import Profile
 from devume.serializers.education_serializer import EducationSerializer
 from devume.authentication.api_key_authentication import ApiKeyAuthentication
+from devume.authentication.bearer_authentication import BearerTokenAuthentication
 
 
 class EducationListView(ListAPIView):
@@ -52,12 +53,7 @@ class EducationUpdateView(UpdateAPIView):
 
 
 class EducationCreateView(CreateAPIView):
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [SessionAuthentication, BearerTokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Education.objects.all()
     serializer_class = EducationSerializer
-
-    def perform_create(self, serializer):
-        # Set the user_id field to the ID of the authenticated user
-        user_profile = Profile.objects.get(user=self.request.user)
-        serializer.save(profile=user_profile)
